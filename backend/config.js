6695185280c65b0b1427bd4ee4a9978c788f7217
shahina-abi <shahinakk300@gmail.com>
2024-08-27 23:51:28 +0400

    Update config.js

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
-export const PORT = 4647
+export const PORT = 3000
 export const mongoDBURL = 'mongodb+srv://shahinakk300:jXqyejFQAFgRObQn@cluster0.pyemi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
